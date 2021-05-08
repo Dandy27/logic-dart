1a4e1bbeade6cca11cc19aee66339181765eb253
dandy <danielsbarbosa27@gmail.com>
2021-05-08 14:01:35 -0300

    Map

--- curso_dart/16-Colecoes/4-Map/Map.dart
+++ curso_dart/16-Colecoes/4-Map/Map.dart
@@ -53,6 +53,72 @@ linkedHashMap() {
   }
 }
 
+///
+///* Map
+///- Map transforma/seleciona os elementos de uma coleção criando uma nova do mesmo tamanho!
+///
+
+mapMap() {
+  print('\n16.4.1) Map map\n');
+
+  List<Map<String, dynamic>> carrinho = [
+    {'nome': 'Borracha', 'preco': 3.45},
+    {'nome': 'Caderno', 'preco': 13.9},
+    {'nome': 'KitLapis', 'preco': 41.22},
+    {'nome': 'Caneta', 'preco': 7.5}
+  ];
+
+  Function porcentagem(desconto) =>
+      (valor) => desconto * valor['preco']; // closure
+  final moeda =
+      (e) => 'RS ${e.toDouble().toStringAsFixed(2).replaceFirst('.', ',')}';
+  List<String> precos1 =
+      carrinho.map(porcentagem(.9) as dynamic).map(moeda).toList();
+  List<String> precos2 =
+      carrinho.map((e) => e.update('preco', (e) => e * .9)).map(moeda).toList();
+  String precoMedio = (carrinho.map((e) => e['preco']).reduce((p, c) => p + c) /
+          carrinho.length)
+      .toStringAsFixed(2);
+  ;
+  print(
+      'Lista de precos1: $precos1\nLista de precos2: $precos2\nPreço Médio: $precoMedio');
+
+  List<Map<dynamic, Object>> escola = [
+    {
+      'nome': 'Turma T1',
+      'alunos': [
+        {'nome': 'Dandy', 'nota': 8.1},
+        {'nome': 'Nayla', 'nota': 9.3}
+      ]
+    },
+    {
+      'nome': 'Turma T2',
+      'alunos': [
+        {'nome': 'Doug', 'nota': 8.9},
+        {'nome': 'Lela', 'nota': 10.0}
+      ]
+    }
+  ];
+
+  // List<dynamic> alunosEscola = escola
+  //     .map((e) => e['alunos'])
+  //     .expand((a) => a as Iterable)
+  //     .map((e) => e['nota'])
+  //     .toList();
+  List<dynamic> alunosEscola =
+      escola.map((e) => e['alunos']).expand((a) => a as Iterable).toList();
+
+  print('Lista Ordenada: $alunosEscola');
+  alunosEscola.sort((b, a) => a['nota'].compareTo(b['nota']));
+  print('Lista Ordenada: $alunosEscola');
+  double mediaNotas =
+      alunosEscola.map((e) => e['nota']).reduce((p, c) => p + c) /
+          alunosEscola.length;
+  print('Média Alunos: ${(mediaNotas).toStringAsFixed(2)}');
+  alunosEscola.forEach((e) => print('${e['nome']} ${e['nota']}'));
+}
+
 void main() {
   linkedHashMap();
+  mapMap();
 }
